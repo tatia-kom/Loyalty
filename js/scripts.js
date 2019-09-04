44fbb8f2bcbf2fe62a9b4980ef75de36dfb092c7
tatia-kom <komarovats93@gmail.com>
2019-09-04 18:39:09 +0300

    feat: add shop screens

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,10 @@ profileBlock.onmouseout = function(event) {
     profileMenu.classList.remove('page__profile-menu--visible');
 }
 
-// edit form
-
 document.addEventListener('DOMContentLoaded', function() {
+
+    // edit form
+
     const edits = document.getElementsByClassName('create-block__clear-input');
 
     for (let i=0; i < edits.length; i++) {
@@ -21,4 +22,29 @@ document.addEventListener('DOMContentLoaded', function() {
             this.previousElementSibling.value = '';
         });
     }
+
+    // open cashiers in shops
+
+    const mores = document.getElementsByClassName('main-blocks__link--more');
+    const shopBlock = document.getElementById('shop-blocks');
+
+    for (let i=0; i < mores.length; i++) {
+        mores[i].addEventListener('click', function(event) {
+            event.stopPropagation();
+            const itemBlock = this.parentElement.parentElement;
+            itemBlock.classList.add('main-blocks__item--opened');
+            itemBlock.style.height = itemBlock.offsetHeight + 'px';
+            itemBlock.firstElementChild.classList.add('main-blocks__item-absolute--active');
+            this.classList.add('main-blocks__link--hidden');
+            this.nextElementSibling.classList.add('more-cashiers--visible');
+            shopBlock.classList.add('main-blocks--opened_item');
+        });
+    }
+
+    document.getElementsByTagName('body')[0].addEventListener('click', function() {
+        shopBlock.classList.remove('main-blocks--opened_item');
+        document.getElementsByClassName('more-cashiers--visible')[0].classList.remove('more-cashiers--visible');
+        document.getElementsByClassName('main-blocks__link--hidden')[0].classList.remove('main-blocks__link--hidden');
+        document.getElementsByClassName('main-blocks__item--opened')[0].classList.remove('main-blocks__item--opened');
+    });
 });
