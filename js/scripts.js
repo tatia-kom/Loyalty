17f46056f5dfba9efdbfcab52234f760c1c5c953
tatia-kom <komarovats93@gmail.com>
2019-09-04 16:46:55 +0300

    feat: add cashiers screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// profile menu
+
 const profileBlock = document.getElementById('header-profile');
 const profileMenu = document.getElementById('header-profile-menu');
 
@@ -8,3 +10,15 @@ profileBlock.onmouseover = function(event) {
 profileBlock.onmouseout = function(event) {
     profileMenu.classList.remove('page__profile-menu--visible');
 }
+
+// edit form
+
+document.addEventListener('DOMContentLoaded', function() {
+    const edits = document.getElementsByClassName('create-block__clear-input');
+
+    for (let i=0; i < edits.length; i++) {
+        edits[i].addEventListener('click', function(event) {
+            this.previousElementSibling.value = '';
+        });
+    }
+});
